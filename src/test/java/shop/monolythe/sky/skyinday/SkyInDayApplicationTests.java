2f0554767551576d404f3dc57fc0070080ba3c66
Vist233 <144699295+Vist233@users.noreply.github.com>
2025-05-27 12:08:44 +0800

    last

--- src/test/java/shop/monolythe/sky/skyinday/SkyInDayApplicationTests.java
+++ src/test/java/shop/monolythe/sky/skyinday/SkyInDayApplicationTests.java
@@ -3,7 +3,6 @@ package shop.monolythe.sky.skyinday;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import shop.monolythe.sky.skyinday.entity.FunctionDef;
 import shop.monolythe.sky.skyinday.repository.FunctionDefRepository;
 
@@ -12,7 +11,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class SkyInDayApplicationTests {
     @Autowired
     private FunctionDefRepository repo;
